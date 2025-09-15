0f638f1d90d996483d93401f9de157040761c958
rigstire <brodsky.nick@gmail.com>
2025-09-15 13:54:00 -0700

    s3 bucket

--- SierraVista/settings.py
+++ SierraVista/settings.py
@@ -113,3 +113,18 @@ STORAGES = {
 
 # ---------- Default PK ----------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if os.getenv("AWS_STORAGE_BUCKET_NAME"):
+    INSTALLED_APPS += ["storages"]
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
+    AWS_DEFAULT_ACL = None
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_QUERYSTRING_AUTH = False  # set True if you want signed URLs
+    # Optional custom domain (CDN or virtual-hosted-style):
+    bucket = os.environ["AWS_STORAGE_BUCKET_NAME"]
+    region = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
+    AWS_S3_CUSTOM_DOMAIN = os.getenv(
+        "AWS_S3_CUSTOM_DOMAIN",
+        f"{bucket}.s3.{region}.amazonaws.com"
+    )
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
